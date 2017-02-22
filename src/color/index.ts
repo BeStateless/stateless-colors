a2f33cd5983850ab8a47c60f050f4ad8594288d5
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-02-22 14:56:02 -0700

    add proper functions to color

--- src/color/index.ts
+++ src/color/index.ts
@@ -1,9 +1,9 @@
-import { darken, lighten, determineColorFormat, rgbToObject } from '../manipulation';
+import { darken, lighten, determineColorFormat, rgbToObject, hexToHSLObject, desaturate, saturate } from '../manipulation';
 import { RGBColorObject, HSLColorObject } from '../interfaces';
 
 
 
-export const Color = (color:string) => {
+export const Color = (color:string | HSLColorObject | RGBColorObject) => {
   this.color = color;
   this.toObject = (objectType: 'rgb' | 'rgba' | 'hsl' | 'hsla'):RGBColorObject | HSLColorObject => {
     const format = determineColorFormat(this.color);
@@ -12,12 +12,28 @@ export const Color = (color:string) => {
         this.color = rgbToObject(this.color);
       }
     }
-
+    if (format === 'hex') {
+      if (objectType === 'hsl') {
+        this.color = hexToHSLObject(this.color);
+      }
+    }
 
     return this;
   };
   this.darken = (percentage: number) => {
     this.color = darken(this.color, percentage);
+    return this;
+  };
+  this.lighten = (percentage: number) => {
+    this.color = lighten(this.color, percentage);
+    return this;
+  };
+  this.saturate = (percentage: number) => {
+    this.color = saturate(this.color, percentage);
+    return this;
+  };
+  this.desaturate = (percentage: number) => {
+    this.color = desaturate(this.color, percentage);
     return this;
   };
 
